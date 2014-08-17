c69284fa93faa8165e52a2b577db5d210b225474
Ayaka Nonaka <ayanonagon@gmail.com>
2014-08-16 23:04:26 -0700

    Add very basic Twitter integration

--- Spreken/TweetsViewController.swift
+++ Spreken/TweetsViewController.swift
@@ -7,96 +7,52 @@
 //
 
 import UIKit
+import Social
+import Accounts
 
 class TweetsViewController: UITableViewController {
+
+    let accountStore: ACAccountStore;
+    let translator: Polyglot
+
+    required init(coder aDecoder: NSCoder!) {
+        accountStore = ACAccountStore()
+        translator = Polyglot(clientId: "", clientSecret: "")
+        super.init(coder: aDecoder)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let translator = Polyglot(clientId: "", clientSecret: "")
-
         var english = "Hallo wereld"
-        translator.translate(english) { translation in
-            println(String(format: "\"%@\" means \"%@\"", english, translation))
-            var english2 = "Ik weet het niet"
-            translator.translate(english2) { translation in
-                println(String(format: "\"%@\" means \"%@\"", english2, translation))
+        self.translateToEnglish("Hallo wereld")
+
+        if (self.userHasAccessToTwitter()) {
+            let twitterAccountType = self.accountStore.accountTypeWithAccountTypeIdentifier(ACAccountTypeIdentifierTwitter)
+            self.accountStore.requestAccessToAccountsWithType(twitterAccountType, options: nil) {
+                granted, error in
+                if granted {
+                    let twitterAccounts = self.accountStore.accountsWithAccountType(twitterAccountType)
+                    if twitterAccounts.count == 0 {
+                        let alertView = UIAlertView(title: "No Twitter accounts found", message: "Please add a Twitter account in Settings.",
+                                                    delegate: nil, cancelButtonTitle: "OK")
+                        alertView.show()
+                    } else {
+                        let twitterAccount = twitterAccounts[0] as ACAccount
+                        // TODO: Do something with this account.
+                    }
+                }
             }
         }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func userHasAccessToTwitter() -> Bool {
+        return SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter)
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView!) -> Int {
-        // #warning Potentially incomplete method implementation.
-        // Return the number of sections.
-        return 0
+    func translateToEnglish(text: String) {
+        self.translator.translate(text) { translation in
+            println(String(format: "\"%@\" means \"%@\"", text, translation))
+        }
     }
-
-    override func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete method implementation.
-        // Return the number of rows in the section.
-        return 0
-    }
-
-    /*
-    override func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView!, canEditRowAtIndexPath indexPath: NSIndexPath!) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView!, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath!) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView!, moveRowAtIndexPath fromIndexPath: NSIndexPath!, toIndexPath: NSIndexPath!) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView!, canMoveRowAtIndexPath indexPath: NSIndexPath!) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
